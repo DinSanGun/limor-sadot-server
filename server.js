83af26134117e85feaa13f9d4b7aeb6009dcf4f5
Din Yair <dinsadot@gmail.com>
2022-05-20 14:43:48 +0300

    Added testing for server

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.json());
 app.use(cors({  origin: 'http://localhost:3000'    }));
 app.use(router)
 
+app.get('/testing',(req,res) => {
+    console.log("It's working in console!")
+    alert("Hey it's working!")
+})
+
 app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running!')
 })
