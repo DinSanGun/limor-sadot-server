728529ab4e8dab2dd59b4a375ddda87f14d9a28e
Din Yair <dinsadot@gmail.com>
2022-06-15 18:19:26 +0300

    Changed server port back to 8080 for testing before SSL cert install

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express();
 app.use('/', express.static("public"));
 app.use(express.json());
 app.use(cors());
-app.use(router)
+app.use(router);
+const port = 8080;
 
 app.get('/testing',(req,res) => {
     res.send("Hey it's working!")
 })
 
-app.listen(443, () => {
+app.listen(port, () => {
     console.log('Server is running!')
 })
