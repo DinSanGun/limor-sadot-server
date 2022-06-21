4484f1b4b7f0482f85852766a324edb400affff6
Din Yair <dinsadot@gmail.com>
2022-06-21 18:19:31 +0300

    Validating SSL certificate

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use('/', express.static("public"));
 app.use(express.json());
 app.use(cors());
 app.use(router);
-const port = 443;
+const port = 80;
+const validationFilePath = '/.well-known/pki-validation/5B5B1A2F9211E2759DD26D5E97AA1C04.txt'
 
 app.get('/', (req,res) => {
     res.send('Hello from SSL Server!')
@@ -24,15 +25,19 @@ app.get('/testing',(req,res) => {
     res.send("Hey it's working!")
 })
 
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(dirname, 'cert', 'cert.pem'))
-}, app)
-
-sslServer.listen(port, () => {
-    console.log('Secure server is up on port 443.')
+app.get(validationFilePath, (req,res) => {
+    res.sendFile(dirname + validationFilePath);
 })
 
-// app.listen(port, () => {
-//     console.log('Server is running!')
+// const sslServer = https.createServer({
+//     key: fs.readFileSync(path.join(dirname, 'cert', 'key.pem')),
+//     cert: fs.readFileSync(path.join(dirname, 'cert', 'cert.pem'))
+// }, app)
+
+// sslServer.listen(port, () => {
+//     console.log('Secure server is up on port 443.')
 // })
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
+})
