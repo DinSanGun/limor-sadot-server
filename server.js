a5d2abb0b5229e3b7de69fc9e1739fcd8e19a8a5
Din Yair <dinsadot@gmail.com>
2022-06-26 22:51:40 +0300

    Preparing server for SSL certificate installation

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use('/', express.static("public"));
 app.use(express.json());
 app.use(cors());
 app.use(router);
-const port = 80;
-const validationFilePath = '/.well-known/pki-validation/5B5B1A2F9211E2759DD26D5E97AA1C04.txt'
+const port = 443;
+// const validationFilePath = '/.well-known/pki-validation/5B5B1A2F9211E2759DD26D5E97AA1C04.txt'
 
 app.get('/', (req,res) => {
     res.send('Hello from SSL Server!')
@@ -25,19 +25,23 @@ app.get('/testing',(req,res) => {
     res.send("Hey it's working!")
 })
 
-app.get(validationFilePath, (req,res) => {
-    res.sendFile(dirname + validationFilePath);
-})
-
-// const sslServer = https.createServer({
-//     key: fs.readFileSync(path.join(dirname, 'cert', 'key.pem')),
-//     cert: fs.readFileSync(path.join(dirname, 'cert', 'cert.pem'))
-// }, app)
-
-// sslServer.listen(port, () => {
-//     console.log('Secure server is up on port 443.')
+// app.get(validationFilePath, (req,res) => {
+//     res.sendFile(dirname + validationFilePath);
 // })
 
-app.listen(port, () => {
-    console.log('Server is running on port ' + port)
+const options = {
+    key: fs.readFileSync(dirname + '/cert/limorsadot.link_key.key'),
+    cert: fs.readFileSync(dirname + '/cert/limorsadot.link.crt'),
+    ca: fs.readFileSync(dirname + '/cert/limorsadot.link.ca-bundle')
+}
+
+const sslServer = https.createServer(options, app)
+
+sslServer.listen(port, () => {
+    console.log('Secure server is up on port ' + port)
 })
+
+// app.listen(port, () => {
+//     console.log('Server is running on port ' + port)
+//     console.log(dirname)
+// })
